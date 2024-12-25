fe4ab9922fcae082fe73e5a616276a22b95c361a
nixzhu <zhuhongxu@gmail.com>
2024-12-25 13:20:45 +0800

    update Ananda 0.12.0

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/nixzhu/Ananda.git",
-            from: "0.11.0"
+            from: "0.12.0"
         ),
         .package(
             url: "https://github.com/swiftlang/swift-syntax.git",
